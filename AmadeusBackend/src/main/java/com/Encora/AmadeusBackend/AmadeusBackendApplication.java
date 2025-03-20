7c711b853c7e6aa12cac139915f1cfd842725a44
Diego.Hernandez <diego.hernandez@ENCEDCCUULT0753.local>
2025-03-20 10:21:51 -0600

    First commit

---
+++ AmadeusBackend/src/main/java/com/Encora/AmadeusBackend/AmadeusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Encora.AmadeusBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmadeusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmadeusBackendApplication.class, args);
+	}
+
+}
---
+++ AmadeusBackend/src/test/java/com/Encora/AmadeusBackend/AmadeusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Encora.AmadeusBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmadeusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
